6c869d90e086581795e032a9213df894ba3908df
ankitkumar <ankitkumar5049@gmail.com>
2025-02-27 01:13:17 +0530

    Minor issue fix

--- app/src/main/java/com/example/chattogether/MainActivity.kt
+++ app/src/main/java/com/example/chattogether/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.chattogether.navigation.Navigation
 import com.example.chattogether.screens.Dashboard
 import com.example.chattogether.navigation.Screen
 import com.example.chattogether.screens.ChatScreen
@@ -18,46 +19,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            val navController = rememberNavController()
-
-            NavHost(
-                navController = navController,
-                startDestination = Screen.Login.route
-            ) {
-                composable(Screen.Login.route) {
-                    LoginScreen(
-                        navController = navController,
-                        onLoginSuccess = { navController.navigate(Screen.Main.route){
-                            popUpTo(Screen.Login.route) { inclusive = true }
-                        } },
-                        onSignUpClick = { navController.navigate(Screen.SignUp.route){
-                            popUpTo(Screen.Login.route) { inclusive = true }
-                        } }
-                    )
-                }
-                composable(route = Screen.SignUp.route) {
-                    SignUpScreen(
-                        navController = navController,
-                        onLoginClick = {navController.navigate(Screen.Login.route){
-                            popUpTo(Screen.SignUp.route) { inclusive = true }
-                        } }
-                    )
-                }
-                composable(Screen.Main.route) {
-                    Dashboard(navController)
-                }
-
-                composable(route = Screen.Chats.route) { backStackEntry ->
-                    val currentUserId = backStackEntry.arguments?.getString("currentUserId") ?: ""
-                    val otherUserId = backStackEntry.arguments?.getString("otherUserId") ?: ""
-
-                    ChatScreen(
-                        navController = navController,
-                        userId = currentUserId,
-                        otherUserId = otherUserId
-                    )
-                }
-            }
+            Navigation()
         }
     }
 }
