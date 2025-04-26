bc6279eb26c51dc452de6f83f8c0d380a542852e
ankitkumar <ankitkumar5049@gmail.com>
2025-04-26 23:49:53 +0530

    minor fix

--- app/src/main/java/com/example/chattogether/utils/Constant.kt
+++ app/src/main/java/com/example/chattogether/utils/Constant.kt
@@ -11,6 +11,7 @@ object Constant {
     const val CHATS = "chats"
     const val USERS = "users"
     const val SECRET = "qW8eR!2#tY6uIoLp"
-    const val HUGGING_KEY = "Bearer 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"
+//    const val HUGGING_KEY = "Bearer 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"
+    const val HUGGING_KEY = ""
 
 }
